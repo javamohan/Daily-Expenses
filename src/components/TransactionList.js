a8cec3993d596b4b37cacb6d62ca480d87b9c044
Mohan Dega <MDEGA@global.sial.com>
2021-01-04 20:23:09 +0530

    Expenses commit

---
+++ src/components/TransactionList.js
@@ -0,0 +1,16 @@
+import React,{ useContext } from 'react'
+import { GlobalContext } from '../context/Globalstate'
+import { Transaction } from './Transaction';
+
+export const TransactionList = () => {
+    const {transactions } = useContext(GlobalContext);
+    console.log(transactions)
+    return (
+        <div>
+        <h3>History</h3>
+        <ul  className="list">
+            { transactions.map(transaction => ( <Transaction key={transaction.id} transaction ={transaction}/>) )}
+      </ul>
+        </div>
+    )
+}
